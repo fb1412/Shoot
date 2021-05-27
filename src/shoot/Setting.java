743eb2e2dac2e68db934c1acbef79ba3ed80d789
dn <fb1412@qq.com>
2021-05-27 11:05:13 +0800

    setting.jaon是否存在验证

--- src/shoot/Setting.java
+++ src/shoot/Setting.java
@@ -7,87 +7,90 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 
-public class Setting {
+public class Setting{
     public static int Interval;
     public static int SkySpeed;
     public static int EnemiesFrequency;
-
+    
     public static int HeroLife;
     public static int HeroFire;
     public static int Bullet;
     public static int BulletSpeed;
-
+    
     public static int BeeFrequency;
     public static int BeeLife;
     public static int BeeaddFire;
     public static int BeeXSpeed;
     public static int BeeYSpeed;
-
+    
     public static int WaspFrequency;
     public static int WaspLife;
     public static int WaspAddLife;
     public static int WaspXSpeed;
     public static int WaspYSpeed;
-
+    
     public static int BigAirplaneFrequency;
     public static int BigAirplaneLife;
     public static int BigAirplaneScore;
     public static int BigAirplaneSpeed;
-
+    
     public static int AirplaneLife;
     public static int AirplaneScore;
     public static int AirplaneSpeed;
-
-    static {
+    
+    static{
         Setting json = new Setting("./setting.json"); // 这里填入文件路径
         JSONObject env = json.getJson();
-
+        
         Interval = env.getInteger("interval");
         SkySpeed = env.getInteger("skySpeed");
-
+        
         JSONObject hero = env.getJSONObject("Hero");
         HeroLife = hero.getInteger("life");
         HeroFire = hero.getInteger("fire");
         Bullet = hero.getInteger("Bullet");
         BulletSpeed = hero.getInteger("BulletSpeed");
-
+        
         JSONObject enemies = env.getJSONObject("enemies");
         EnemiesFrequency = enemies.getInteger("frequency");
-
+        
         JSONObject bee = enemies.getJSONObject("Bee");
         BeeFrequency = bee.getInteger("frequency");
         BeeLife = bee.getInteger("life");
         BeeaddFire = bee.getInteger("addFire");
         BeeXSpeed = bee.getInteger("xSpeed");
         BeeYSpeed = bee.getInteger("YSpeed");
-
+        
         JSONObject Wasp = enemies.getJSONObject("Wasp");
         WaspFrequency = Wasp.getInteger("frequency");
         WaspLife = Wasp.getInteger("life");
         WaspAddLife = Wasp.getInteger("addLife");
         WaspXSpeed = Wasp.getInteger("xSpeed");
         WaspYSpeed = Wasp.getInteger("YSpeed");
-
+        
         JSONObject bigAirplane = enemies.getJSONObject("BigAirplane");
         BigAirplaneFrequency = bigAirplane.getInteger("frequency");
         BigAirplaneLife = bigAirplane.getInteger("life");
         BigAirplaneScore = bigAirplane.getInteger("score");
         BigAirplaneSpeed = bigAirplane.getInteger("Speed");
-
+        
         JSONObject airplane = enemies.getJSONObject("Airplane");
         AirplaneLife = airplane.getInteger("life");
         AirplaneScore = airplane.getInteger("score");
         AirplaneSpeed = airplane.getInteger("Speed");
     }
-
-    Setting() {
+    
+    Setting(){
     }
-
+    
     private String file;
-
-    Setting(String url) {
+    
+    Setting(String url){
         String charset = "utf-8";
         File file = new File(url);
+        if (!file.exists()) {
+            System.out.println(url + "不存在");
+        }
         long fileByteLength = file.length();
         byte[] content = new byte[(int) fileByteLength];
         FileInputStream fileInputStream = null;
@@ -111,8 +114,8 @@ public class Setting {
         }
         this.file = str;
     }
-
-    public JSONObject getJson() {
+    
+    public JSONObject getJson(){
         return (JSONObject) JSONObject.parse(file);
     }
 }
